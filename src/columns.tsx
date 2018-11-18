042318b91e94be36479adb0b120c5802ec99672d
Mark Murray <mark@murray.xyz>
2018-11-18 12:55:46 +0000

    columns component

---
+++ src/columns.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { CSSConsumer } from './css-context'
+import { MQProvider } from './mq-context'
+
+type ColumnsProps = {
+  gap?: number
+  ratios: number[]
+}
+
+const createStyles = (gap: number, ratios: number[]) => ({
+  display: 'grid',
+  gridColumnGap: `${gap}px`,
+  gridTemplateColumns: ratios.map(ratio => `minmax(0, ${ratio}fr)`).join(' '),
+  width: '100%',
+})
+
+const Columns: React.SFC<ColumnsProps> = ({ gap = 0, ratios, children }) => {
+  const total = ratios.reduce((acc, ratio) => acc + ratio)
+  const totalGap = gap * (ratios.length - 1)
+
+  return (
+    <CSSConsumer>
+      {({ css }) => {
+        const wrappedChildren = React.Children.map(children, (child, index) => {
+          const ratio = ratios[index]
+          const mq = (width: number) => width * (total / ratio) + totalGap
+
+          return (
+            <MQProvider key={index} mq={mq}>
+              {child}
+            </MQProvider>
+          )
+        })
+
+        return (
+          <div className={css(createStyles(gap, ratios))}>
+            {wrappedChildren}
+          </div>
+        )
+      }}
+    </CSSConsumer>
+  )
+}
+
+export default Columns
