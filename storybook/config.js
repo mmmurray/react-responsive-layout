ce4f0bd2dde84a49296b7bf1b4f1e1a0cbb250fc
Mark Murray <mark@murray.xyz>
2018-11-18 11:38:26 +0000

    storybook config

--- storybook/config.js
+++ storybook/config.js
@@ -1,4 +1,6 @@
-import { configure } from '@storybook/react'
+import { withOptions } from '@storybook/addon-options'
+import { themes } from '@storybook/components'
+import { addDecorator, configure } from '@storybook/react'
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext)
@@ -7,5 +9,13 @@ function requireAll(requireContext) {
 function loadStories() {
   requireAll(require.context('../src', true, /\.stories\.tsx$/))
 }
+
+addDecorator(
+  withOptions({
+    name: 'react responsive layout',
+    url: 'https://github.com/mmmurray/react-responsive-layout',
+    theme: themes.dark,
+  }),
+)
 
 configure(loadStories, module)
