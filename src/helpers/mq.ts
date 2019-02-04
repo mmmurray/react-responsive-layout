f349c3f9a25a584ef7fb6f5819857fd882df6845
Mark Murray <mark@murray.xyz>
2019-02-04 18:31:44 +0000

    create mq with multiple ranges

--- src/helpers/mq.ts
+++ src/helpers/mq.ts
@@ -4,7 +4,7 @@ const createRange = ({ min, max }: Range): string =>
   `screen and (min-width: ${min}px) and (max-width: ${max}px)`
 
 const createMq = (ranges: Range[]): string => {
-  return `@media ${ranges.map(createRange).join('')}`
+  return `@media ${ranges.map(createRange).join(', ')}`
 }
 
 export default createMq
--- test/unit/mq.test.ts
+++ test/unit/mq.test.ts
@@ -5,3 +5,15 @@ test('single range', () => {
 
   expect(mq).toBe('@media screen and (min-width: 100px) and (max-width: 200px)')
 })
+
+test('multiple ranges', () => {
+  const mq = createMq([
+    { min: 100, max: 200 },
+    { min: 300, max: 400 },
+    { min: 500, max: 600 },
+  ])
+
+  expect(mq).toBe(
+    '@media screen and (min-width: 100px) and (max-width: 200px), screen and (min-width: 300px) and (max-width: 400px), screen and (min-width: 500px) and (max-width: 600px)',
+  )
+})
