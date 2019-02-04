c0dc8f7c2c8bd0e1cd7aa699267daec02d918470
Mark Murray <mark@murray.xyz>
2019-02-04 18:33:23 +0000

    create mq with zero minmum

--- src/helpers/mq.ts
+++ src/helpers/mq.ts
@@ -1,7 +1,10 @@
 type Range = { min: number; max: number }
 
-const createRange = ({ min, max }: Range): string =>
-  `screen and (min-width: ${min}px) and (max-width: ${max}px)`
+const createRange = ({ min, max }: Range): string => {
+  const minPart = min === 0 ? '' : ` and (min-width: ${min}px)`
+
+  return `screen${minPart} and (max-width: ${max}px)`
+}
 
 const createMq = (ranges: Range[]): string => {
   return `@media ${ranges.map(createRange).join(', ')}`
--- test/unit/mq.test.ts
+++ test/unit/mq.test.ts
@@ -17,3 +17,9 @@ test('multiple ranges', () => {
     '@media screen and (min-width: 100px) and (max-width: 200px), screen and (min-width: 300px) and (max-width: 400px), screen and (min-width: 500px) and (max-width: 600px)',
   )
 })
+
+test('zero minimum', () => {
+  const mq = createMq([{ min: 0, max: 200 }])
+
+  expect(mq).toBe('@media screen and (max-width: 200px)')
+})
