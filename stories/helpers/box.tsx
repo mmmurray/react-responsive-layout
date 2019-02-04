89383bfbef7dda20ec64718eec9b930db86274c1
Mark Murray <mark@murray.xyz>
2019-02-04 18:53:05 +0000

    update box to use custom hook

--- stories/helpers/box.tsx
+++ stories/helpers/box.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react'
 import { SFC, useContext } from 'react'
-import { CSSContext, MQContext } from '../../src'
+import { CSSContext, useMediaQuery } from '../../src'
 import useCurrentWidth from './use-current-width'
 
 type BoxProps = {
   maxWidth: number
 }
 
-const createStyles = (maxWidth: number) => ({
-  backgroundColor: '#c51b62',
+const createStyles = (mq: string) => ({
+  backgroundColor: '#1bc567',
   boxShadow: '0 0 0 1px white inset',
   color: 'white',
   height: '100px',
   padding: '10px',
   width: '100%',
-  [`@media (max-width: ${isFinite(maxWidth) ? maxWidth - 1 : 1000000}px)`]: {
-    backgroundColor: '#1bc567',
+  [mq]: {
+    backgroundColor: '#c51b62',
   },
 })
 
 const Box: SFC<BoxProps> = ({ maxWidth }) => {
   const { css } = useContext(CSSContext)
-  const { mq } = useContext(MQContext)
+  const mq = useMediaQuery(maxWidth)
   const { ref, width } = useCurrentWidth<HTMLDivElement>()
 
   return (
-    <div ref={ref} className={css(createStyles(mq(maxWidth)))}>
+    <div ref={ref} className={css(createStyles(mq))}>
       {width}/{maxWidth}
     </div>
   )
